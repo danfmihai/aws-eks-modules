d8101133450bb40e9565a9a01ef6a7b55e1d51b2
mihai <misu3108@yahoo.com>
2021-10-09 00:52:26 +0000

    edited

--- eks.tf
+++ eks.tf
@@ -1,37 +1,35 @@
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
-
-  cluster_version = "1.21"
-  cluster_name    = "my-cluster"
-  vpc_id          = module.vpc.vpc_id
-  subnets         = [module.vpc.private_subnets[0], module.vpc.public_subnets[1]]
-
-  node_groups = {
-    public = {
-      subnets          = [module.vpc.public_subnets[1]]
-      desired_capacity = 1
-      max_capacity     = 5
-      min_capacity     = 1
-
-      instance_type = "t2.small"
-      k8s_labels = {
-        Environment = "public"
-      }
-    }
-
-    private = {
-      subnets          = [module.vpc.private_subnets[0]]
-      desired_capacity = 1
-      max_capacity     = 5
-      min_capacity     = 1
-
-      instance_type = "t2.small"
-      k8s_labels = {
-        Environment = "private"
-      }
-    }
-  }
-
+  cluster_version           = "1.21"
+  cluster_name              = "my-cluster"
+  vpc_id                    = module.vpc.vpc_id
+  subnets                   = [module.vpc.private_subnets[0], module.vpc.public_subnets[1]]
+  cluster_create_timeout    = "25m"
+  cluster_endpoint_private_access = true
+  
+  worker_groups = [
+  { 
+      name                          = "worker-group-1"
+      instance_type                 = "t2.small"
+      additional_userdata           = "echo foo bar"
+      asg_desired_capacity          = 1
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+  },
+  ]
 
 }
 
+resource "aws_security_group" "worker_group_mgmt_one" {
+  name_prefix = "worker_group_mgmt_one"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+    ]
+  }
+}
